3544636260eaf10b9a21167a7c6c7dcc7be426fc
idowuakinde <8018104+idowuakinde@users.noreply.github.com>
2018-05-03 09:40:17 +0100

    Initial check-in of module XamUFirstApp

---
+++ XamUFirstApp/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Xamarin.Forms;
+
+namespace XamUFirstApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var layout = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = 
+                { 
+                    new Label { Text = "Congrats, Idowu. You did it! Xamarin Forms!", HorizontalTextAlignment = TextAlignment.Center }
+                }
+            };
+
+            MainPage = new ContentPage
+            {
+                Content = layout
+            };
+
+            Button button = new Button
+            {
+                Text = "Click Me"
+            };
+
+            button.Clicked += async (s, e) => 
+            { 
+                await MainPage.DisplayAlert("Alert", "You clicked me", "Ok"); 
+            };
+
+            layout.Children.Add(button);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
